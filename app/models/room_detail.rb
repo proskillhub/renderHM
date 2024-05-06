9057d6ea9b9144307967160a218fdcbb8925353a
AnchalopOjha <iamanchal.ojha@gmail.com>
2024-05-06 15:57:21 +0530

    6th may Afternoon work

--- app/models/room_detail.rb
+++ app/models/room_detail.rb
@@ -1,4 +1,10 @@
 class RoomDetail < ApplicationRecord
   belongs_to :room_type
   enum roomstatus: { dirty: 0, clean: 1 }
+  enum booking_status: {
+    available: 0,  # This should match the expected value
+    booked: 1,
+    reserved: 2,
+    maintenance: 3
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_06_073827) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_06_095146) do
   create_table "beds", force: :cascade do |t|
     t.string "bed_size"
     t.decimal "price"
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_06_073827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "roomstatus"
+    t.integer "booking_status"
     t.index ["room_type_id"], name: "index_room_details_on_room_type_id"
   end
 
